89d2c7d8168197a00528028415c692ee5cf81f41
ernestognw <ernestognw@gmail.com>
2019-04-12 11:16:35 -0500

    [Remove] Ignore of build

---
+++ build/precache-manifest.e1740de5d94bdc4d6ff73ff4b4004543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab45c5fb63c37479d069e7e82d7b99c",
+    "url": "/static/media/logo-white.fab45c5f.svg"
+  },
+  {
+    "revision": "509fecdb41e4243a518c807a48c6857f",
+    "url": "/static/media/header-elements.509fecdb.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6da6cd6ed8e454fe825",
+    "url": "/static/js/main.0842dc6f.chunk.js"
+  },
+  {
+    "revision": "8907ae3249da2269815c",
+    "url": "/static/js/2.1dbbb02f.chunk.js"
+  },
+  {
+    "revision": "8907ae3249da2269815c",
+    "url": "/static/css/2.72dc1002.chunk.css"
+  },
+  {
+    "revision": "bb169f1dc4e5ffd42d0f5135dda2cd54",
+    "url": "/index.html"
+  }
+];
